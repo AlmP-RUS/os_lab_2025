cb40c840c58b4f5e8323950a9a1bbfcde1a72e6c
AlmP-RUS <79363684+AlmP-RUS@users.noreply.github.com>
2026-09-15 14:18:17 +0000

    lr2 complete

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
+#include <string.h>
 #include "revert_string.h"
+
+#define SWAP(a, b) {a^=b;b^=a;a^=b;};
 
 void RevertString(char *str)
 {
-	// your code here
+	size_t len = strlen(str);
+	for (size_t i = 0; i < len / 2; i++)
+	{
+		SWAP(str[i], str[len - i - 1]);
+	}
 }
 
